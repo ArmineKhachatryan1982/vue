454eabe7e5b7eb4a62cb8a548746687cc2f93e21
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-06-24 17:44:30 +0400

    444

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,3 +1,33 @@
-<template>
-    <h3>77777777777777777777</h3>
-</template>>
+<script setup>
+defineProps({
+    title: String,
+    imageUrl: String,
+    price: Number,
+    isFavorite:Boolean,
+    isAdded:Boolean,
+    onClickAdd:Function,
+    onClickFavorite:Function,
+   
+})
+
+</script>
+   <template>
+    <div class="relative bg-white border  border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
+                <img 
+                    :src="!isFavorite ? '/like-2.svg' : '/like-1.svg'" 
+                    alt="like"
+                    class="absolute top-8 left-8"
+                    @click="onClickFavorite"
+                    />
+                <img :src="imageUrl" alt="Sneaker"/>
+                <p class="mt-2">{{title}} </p>
+            
+                <div class="flex justify-between mt-5">
+                    <div class="flex flex-col">
+                        <span class="text-slate-400">Цена:</span>
+                        <b>{{ price }}руб.</b>
+                    </div>
+                    <img  :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
+                </div>
+                </div>
+</template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Card from './Card.vue'
-import CardItem from './CardItem.vue'
+const onClickAdd=()=>{
+  alert('added')
+}
 
 
 
@@ -8,45 +10,15 @@ import CardItem from './CardItem.vue'
 
 <template>
   <div class="grid grid-cols-4 gap-5" >
-   <CardItem />
-   <Card />
+  
+    <!-- <Card 
+        title="Мужские Красожки Нике"
+        imageUrl="\sneakers\sneakers-1.jpg" 
+        :price="1205 " 
+        :is-added="false"
+        :isFavorite="false"
+        :onclickAdd="onClickAdd"
+    /> -->
   </div>
+
 </template>
-// <script setup>
-    // defineProps({
-    //     title: String,
-    //     imageUrl: String,
-    //     price: Number,
-    //     isFavorite:Boolean,
-    //     isAdded:Boolean,
-       
-    // })
-   
-</script>
-
-<template>
-    <div class="relative bg-white border  border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
-                <!-- <img :src="!isFavorite ? '/like-2.svg' : '/like-1.svg'" alt="like" class="absolute top-8 left-8"/> -->
-                <!-- <img :src="imageUrl" alt="Sneaker"/>
-                <p class="mt-2">{{title}} </p>
-            
-                <div class="flex justify-between mt-5">
-                    <div class="flex flex-col">
-                        <span class="text-slate-400">Цена:</span>
-                        <b>{{ price }}руб.</b>
-                    </div>
-                    <img  :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
-                </div> -->
-                </div>
-</template>
-
-
-
- <Card  
-                title="Мужские Красожки Нике"
-                imageUrl="\sneakers\sneakers-1.jpg" 
-                :price="1205 " 
-                :is-added="false"
-                :isFavorite="false"
-      
-                />
